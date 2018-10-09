7308db50f631acaa07e19bda2925fddcbe04f680
Rodelio <odedahay@gmail.com>
2018-10-09 09:04:41 +0800

    Update Event Components

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
-import {Grid} from 'semantic-ui-react';
+import {Grid, Button } from 'semantic-ui-react';
+import EventList from "../EventList/EventList";
+import EventForm from '../EventForm/EventForm';
 
 class EventDashboard extends Component {
   render() {
     return (
       <Grid>
         <Grid.Column width={10}>
-            <h2>Left Col</h2>
+          <EventList/>
         </Grid.Column>
         <Grid.Column width={6}>
-                <h2>Right Col</h2>
-            </Grid.Column>
+          <Button positive content='Create Event'/>
+          <EventForm />
+        </Grid.Column>
       </Grid>
     )
   }
